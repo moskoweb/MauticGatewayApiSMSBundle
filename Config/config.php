546416a96bc96b080471ada824e719c3b0a3de01
Alan Mosko <falecom@alanmosko.com.br>
2019-10-13 12:11:44 -0300

    wip

--- Config/config.php
+++ Config/config.php
@@ -16,8 +16,7 @@ return [
     'version'     => '1.0.0',
     'services' => [
         'events'  => [],
-        'forms'   => [
-        ],
+        'forms'   => [],
         'helpers' => [],
         'other'   => [
             'mautic.sms.transport.gatewayapi' => [
@@ -43,7 +42,24 @@ return [
         ],
     ],
     'routes'     => [],
-    'menu'       => [],
-    'parameters' => [
+    'menu' => [
+        'main' => [
+            'items' => [
+                'mautic.sms.smses' => [
+                    'route'  => 'mautic_sms_index',
+                    'access' => ['sms:smses:viewown', 'sms:smses:viewother'],
+                    'parent' => 'mautic.core.channels',
+                    'checks' => [
+                        'integration' => [
+                            'GatewayApi' => [
+                                'enabled' => true,
+                            ],
+                        ],
+                    ],
+                    'priority' => 70,
+                ],
+            ],
+        ],
     ],
+    'parameters' => [],
 ];
